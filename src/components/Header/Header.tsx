caf0c3d98765b9f60b7d47c21cf1a9ea09cacb62
Mister Prikol <public@main.ru>
2025-10-02 13:14:59 +0300

    fix: fixed links in header from <a> to <link> for better rendering

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import style from "./Header.module.css";
 import {useAppSelector} from "../../store/hooks.ts";
 import Sidebar from "../Sidebar";
+import {Link} from "react-router-dom";
 
 const Header: React.FC = () => {
     const cartItems = useAppSelector((state) => state.cart.items);
@@ -14,10 +15,10 @@ const Header: React.FC = () => {
             <header className={style.header}>
                 <h1 className={style.h1}>Performance Lab Store</h1>
                 <nav className={style.nav}>
-                    <a className={style.navLink} href="/">Главная</a>
-                    <a className={style.navLink} href="/category/food">Еда</a>
-                    <a className={style.navLink} href="/category/clothes">Одежда</a>
-                    <a className={style.navLink} href="/category/electronics">Электроника</a>
+                    <Link className={style.navLink} to="/">Главная</Link>
+                    <Link className={style.navLink} to="/category/food">Еда</Link>
+                    <Link className={style.navLink} to="/category/clothes">Одежда</Link>
+                    <Link className={style.navLink} to="/category/electronics">Электроника</Link>
 
                     <div className={style.cartIcon} onClick={() => setIsCartOpen(true)}>
                         🛒 {totalItems > 0 && <span className={style.cartCount}>{totalItems}</span>}
