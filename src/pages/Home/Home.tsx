415cdb92c15752a49166d9c305c83a3fe28f4272
Mister Prikol <public@main.ru>
2025-10-02 12:18:48 +0300

    style: add basic styling for home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import style from './Home.module.css';
 
 const Home: React.FC = () => {
     return (
-        <div>
-            <h2>Добро пожаловать в наш магазин!</h2>
-            <p>Выберите категорию товаров в меню ниже</p>
+        <div className={style.home}>
+            <h2 className={style.title}>Добро пожаловать в наш магазин!</h2>
+            <p className={style.subtitle}>Выберите категорию товаров в меню выше</p>
         </div>
     );
 };
